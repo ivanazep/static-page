71852f90f64b62cf13571eeda38dcec40d3526d0
IvanaUr <iurosevic@renderedtext.com>
2018-03-09 10:59:05 +0100

    change app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8000);
-});
+    fs = require('fs')
+var port = process.env.PORT || 3000
+http.createServer(function(req, res) {
+    var url = './' + (req.url == '/' ? 'index.html' : req.url)
+    fs.readFile(url, function(err, html) {
+        if (err) {
+            var message404 = "There is no such page! <a href='/'>Back to home page</a>"
+            res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
+            res.write(message404)
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': html.length})
+            res.write(html)
+        }
+        res.end()
+    })
+}).listen(port)
